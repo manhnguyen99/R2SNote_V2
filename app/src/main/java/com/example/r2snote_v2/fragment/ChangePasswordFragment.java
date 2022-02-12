95e927b8c2baef85d1bb8388ffd3620ea33edf95
Minhlv000 <minhlevan1003@gmail.com>
2022-02-12 22:42:47 +0700

    create navigation drawer and fragment

---
+++ app/src/main/java/com/example/r2snote_v2/fragment/ChangePasswordFragment.java
@@ -0,0 +1,28 @@
+package com.example.r2snote_v2.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.r2snote_v2.R;
+
+
+public class ChangePasswordFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_change_password,container,false);
+        return view;
+    }
+}
